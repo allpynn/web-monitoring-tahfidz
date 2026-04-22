0003f2bd781b4fe3801558379f2327deceeff11e
17KyshnNostra <choirulnarizky89@gmail.com>
2026-04-22 19:37:04 +0700

    chore: configure vite server host and hmr settings for external access

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
         }),
     ],
     server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
         watch: {
             usePolling: true,
         },
