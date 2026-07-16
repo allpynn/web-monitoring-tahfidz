b3d24400982e65ea07020db27220cbfcf697deb9
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2026-07-16 19:11:42 +0700

    fix: route relative

--- resources/views/parent/dashboard.blade.php
+++ resources/views/parent/dashboard.blade.php
@@ -205,7 +205,7 @@
                             
                             <form id="chat-form-{{ $student->id }}"
                                   data-student-id="{{ $student->id }}"
-                                  data-action="{{ route('parent.messages.send', $student) }}"
+                                  data-action="{{ route('parent.messages.send', $student, false) }}"
                                   class="flex gap-3 bg-white dark:bg-gray-900 border border-gray-100 dark:border-gray-700 p-3 rounded-2xl">
                                 @csrf
                                 <input type="text" name="message" required placeholder="Tulis pesan ke Ustadz..."
